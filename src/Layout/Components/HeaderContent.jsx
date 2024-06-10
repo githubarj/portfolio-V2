6ccbe618ba7334f64477c76ef013d1dc8f1f4c57
Richard Githuba <githuba9520@gmail.com>
2024-06-11 00:31:43 +0300

    styling fixes on the hader, name area converted to using rem for header padding

--- src/Layout/Components/HeaderContent.jsx
+++ src/Layout/Components/HeaderContent.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Flex, Row } from 'antd';
+import { Col, Row } from 'antd';
 import { Typography } from 'antd';
 
 const { Text } = Typography;
@@ -6,10 +6,10 @@ const { Text } = Typography;
 function HeaderContent() {
   return (
     <Row className='header-content'>
-      <Col span={4} className='header-content__name'>
-        <Text level={5}>Githuba R Jeremy</Text>
+      <Col span={6} className='header-content__name'>
+        <Text>githuba-jeremy</Text>
       </Col>
-      <Col span={19} className='header-content__menus'></Col>
+      <Col span={16} className='header-content__menus'></Col>
     </Row>
   );
 }
