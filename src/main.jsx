8ebd9180d078050efe7e2fe7d167e57b5ac86304
Richard Githuba <githuba9520@gmail.com>
2024-06-11 12:07:56 +0300

    added react redux to the project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client' 
 import App from './App.jsx'
 import './sass/index.scss'
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
       <App />
-    </React.StrictMode>
+    </Provider>
+  </React.StrictMode>
 );
