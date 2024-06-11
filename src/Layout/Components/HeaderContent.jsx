a45e8cdfb76e2ca3f1ad2bba62b25374a2e5146e
Richard Githuba <githuba9520@gmail.com>
2024-06-11 22:41:13 +0300

    done with the header with full responsiveness

--- src/Layout/Components/HeaderContent.jsx
+++ src/Layout/Components/HeaderContent.jsx
@@ -1,12 +1,27 @@
-import { Col, ConfigProvider, Menu, Row, Typography } from 'antd';
+import { Col, ConfigProvider, Dropdown, Menu, Row, Typography } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { navTo } from '../../redux/slices/navSlice';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { useEffect, useState } from 'react';
 
 const { Text } = Typography;
 
 function HeaderContent() {
   const current = useSelector((state) => state.currentPage.value); // Ensure state path is correct
   const dispatch = useDispatch();
+  const [burgerMenu, setBurgerMenu] = useState(window.innerWidth <= 581);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBurgerMenu(window.innerWidth <= 581);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const items = [
     { label: '_hello', key: 'hello' },
@@ -34,18 +49,36 @@ function HeaderContent() {
       }}
     >
       <Row className='header-content'>
-        <Col span={6} className='header-content__name'>
+        <Col
+          sm={6}
+          xs={20}
+          className={`header-content__name ${burgerMenu && 'no-border'}`}
+        >
           <Text>githuba-jeremy</Text>
         </Col>
-        <Col span={18} className='header-content__menus'>
-          <Menu
-            className='menu-container'
-            onClick={handleClick}
-            selectedKeys={[current]}
-            mode='horizontal'
-            items={items}
-          />
-         
+        <Col
+          sm={18}
+          xs={4}
+          className={`header-content__menus ${burgerMenu && 'burger'}`}
+        >
+          {burgerMenu ? (
+            <Dropdown
+              trigger={['click']}
+              menu={{ items, selectable: true, defaultSelectedKeys: ['hello'] }}
+              placement='bottomRight'
+              arrow={{ pointAtCenter: true }}
+            >
+              <RxHamburgerMenu fontSize={24} />
+            </Dropdown>
+          ) : (
+            <Menu
+              className='menu-container'
+              onClick={handleClick}
+              selectedKeys={[current]}
+              mode='horizontal'
+              items={items}
+            />
+          )}
         </Col>
       </Row>
     </ConfigProvider>
