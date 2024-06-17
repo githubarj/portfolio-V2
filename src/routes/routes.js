fd388a7f73f50f7a9a3f28f97dc5aeb101281051
Richard Githuba <githuba9520@gmail.com>
2024-06-18 01:45:27 +0300

    lazyload hello

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import AppLayout from '../Layout/Index';
-import Home from '../views/Home/Index';
+// import Home from '../views/Home/Index';
 import Projects from '../views/Projects/Index';
 import Contact from '../views/Contact/Index';
 import About from '../views/About/Index';
@@ -8,6 +8,18 @@ import { redirect } from 'react-router-dom';
 import Bio from '../views/About/Components/Bio/Bio';
 import Experience from '../views/About/Components/Experience/Experience';
 import Education from '../views/About/Components/Education/Education';
+import { Backdrop, CircularProgress } from '@mui/material';
+
+const Home = lazy(() => import('../views/Home/Index'));
+
+const fallback = React.createElement(
+  Backdrop,
+  {
+    sx: { color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 },
+    open: true,
+  },
+  React.createElement(CircularProgress, { color: 'inherit' })
+);
 
 export const routes = [
   {
@@ -20,10 +32,13 @@ export const routes = [
       },
       {
         path: 'hello',
-        element: React.createElement(Home),
+        element: React.createElement(
+          Suspense,
+          { fallback },
+          React.createElement(Home)
+        ),
       },
       {
-       
         path: 'about',
         element: React.createElement(About),
         children: [
