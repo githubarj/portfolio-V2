0ef4d0ff72fc8b40ed0f51204c5b25274bf57b10
Richard Githuba <githuba9520@gmail.com>
2024-06-17 23:39:20 +0300

    some styling chnages

--- src/views/Home/Index.jsx
+++ src/views/Home/Index.jsx
@@ -1,14 +1,16 @@
-import { Col, Row } from "antd"
+import { Col, Row } from 'antd';
 import Intro from './Components/Intro';
 const Home = () => {
   return (
-    <Row className="home-container" >
-      <Col className="intro-section" >
+    <Row
+      className='home-container'
+     
+    >
+      <Col className='intro-section'>
         <Intro />
       </Col>
-      
     </Row>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
