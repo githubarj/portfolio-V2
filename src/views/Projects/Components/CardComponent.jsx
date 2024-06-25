9b2f2a26a90bfe6856046e4431d252693b6ccbab
Richard Githuba <githuba9520@gmail.com>
2024-06-25 21:07:35 +0300

    added framer motion to the project

--- src/views/Projects/Components/CardComponent.jsx
+++ src/views/Projects/Components/CardComponent.jsx
@@ -1,5 +1,5 @@
-import { Button, Card, Col, Flex, Skeleton, Tag, Typography } from 'antd';
-import React, { Suspense, useEffect, useState } from 'react';
+import {  Card, Col, Flex, Tag, Typography } from 'antd';
+import React from 'react';
 import { filterIcons } from './filterIcons';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -8,13 +8,6 @@ import { v4 as uuidv4 } from 'uuid';
 const { Text, Link } = Typography;
 
 const CardComponent = (props) => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 200);
-  }, []);
-
   const { description, category, title, link, image } = props;
   const layout = {
     lg: 8,
@@ -26,66 +19,45 @@ const CardComponent = (props) => {
   const categories = filterIcons.filter((item) => category.includes(item.key));
 
   return (
-    <Col className='projects-container__columns' {...layout}>
-      {loading ? (
-        <Skeleton active />
-      ) : (
-        <>
-          <Skeleton
-            active
-            style={{
-              display: loading ? 'block' : 'none',
-              position: 'absolute',
-            }}
-          />
+    
+      <Col className='projects-container__columns' {...layout}>
+       
+        <Flex justify='space-between' align='center'>
+          <Text> {title} </Text>
+          <div className='icons'>
+            {categories.map((item) => (
+              <Tag
+                icon={React.createElement(item.icon, { color: item.color })}
+                key={uuidv4()}
+                bordered={false}
+              ></Tag>
+            ))}
+          </div>
+        </Flex>
+        <Card
+          bordered={false}
+          size='small'
+          className='projects-card'
+          hoverable
+          cover={
+            <div
+              style={{
+                height: '120px',
+                backgroundImage: `url(${image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center top',
+              }}
+            />
+          }
+        >
+          <Text style={{ color: 'silver' }}>{description}</Text>
 
-          <Flex
-            justify='space-between'
-            align='center'
-            style={{
-              visibility: !loading ? 'visible' : 'hidden',
-            }}
-          >
-            <Text> {title} </Text>
-            <div className='icons'>
-              {categories.map((item) => (
-                <Tag
-                  icon={React.createElement(item.icon, { color: item.color })}
-                  key={uuidv4()}
-                  bordered={false}
-                ></Tag>
-              ))}
-            </div>
-          </Flex>
-          <Card
-            style={{
-              visibility: !loading ? 'visible' : 'hidden',
-            }}
-            bordered={false}
-            size='small'
-            className='projects-card'
-            hoverable
-            cover={
-              <div
-                style={{
-                  height: '120px',
-                  backgroundImage: `url(${image})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center top',
-                }}
-              />
-            }
-          >
-            <Text style={{ color: 'silver' }}>{description}</Text>
-
-            <Link href={link} target='_blank' className='link' type='link'>
-              link &gt;
-            </Link>
-          </Card>
-          {/* </Suspense> */}
-        </>
-      )}
-    </Col>
+          <Link href={link} target='_blank' className='link' type='link'>
+            link &gt;
+          </Link>
+        </Card>
+       
+      </Col>
   );
 };
 
