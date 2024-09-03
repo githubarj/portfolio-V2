eaa0d9f8066ebf7b04601ba95d5dfeac717960b2
Richard Githuba <githuba9520@gmail.com>
2024-09-03 14:24:29 +0300

    link change

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -10,7 +10,7 @@ const projectsData = [
   {
     image: '/assets/projectImages/changia.png',
     title: 'Changia',
-    link: 'https://changiake.org/home',
+    link: 'https://changiake.netlify.app/home',
     category: ['react', 'sass', 'ant-design', 'tailwind', 'typescript'],
     description: `WIP. Startup that seeks to connect various residential care facilities with donors and volunteers, designed in figma implemented in react`,
   },
